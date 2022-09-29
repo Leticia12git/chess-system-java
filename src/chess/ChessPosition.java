0f43c891581f36f70ea3d40ae03a4f137ef46825
Leticia12 <leticia_lele.lima@hotmail.com>
2022-09-29 09:59:51 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,20 +9,16 @@ public class ChessPosition {
 
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-<<<<<<< HEAD
+
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
-=======
+
 			throw new ChessException("Error instantiating ChessPosition . Valid values are from a1 to h8.");
 		}
 
-		this.column = column;
-		this.row = row;
-
->>>>>>> 5771b13262b9c8fec0d77c0e507e48f7808818d4
-	}
+	
 
 	public char getColumn() {
 		return column;
@@ -35,19 +31,12 @@ public class ChessPosition {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-<<<<<<< HEAD
 
 	protected static ChessPosition fromPosition(Position position) {
 		return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
 	}
 
-=======
 	
-	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow() );
-	}
-	
->>>>>>> 5771b13262b9c8fec0d77c0e507e48f7808818d4
 	@Override
 	public String toString() {
 		return "" + column + row;
